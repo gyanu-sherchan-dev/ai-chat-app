1909f5866c598af5daa05908cf68853ff6dea6b1
Gyanu <gyanusherchan@outlook.com>
2023-04-10 23:28:31 +1000

    register page input

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div className="bg-blue-50 h-screen">dfdfd</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto mb-12">
+        <input
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button className="bg-blue-500 text-white-block w-full rounded-sm p-2">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
